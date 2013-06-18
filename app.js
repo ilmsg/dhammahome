1333eec3ab90392ede4da0135fe427a00e746136
ilmsg <eak.netpanya@ilmsg.com>
2013-06-18 13:41:30 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = angular.module('dhammahome', []);
+
+app.controller('MainCtrl', function($scope) {
+	$scope.mp3_folder = 'mp3/';
+	$scope.mp3_lists = [];
+	
+	$scope.mp3_lists.push({title: 'กล่าวเกริ่นนำ', file:'00 กล่าวเกริ่นนำ ธ.ต.mp3'});
+	for(var num = 1; num <= 85; num++){
+		var file_name = 'ตอนที่ ' + addZ(num) + '.mp3';
+		$scope.mp3_lists.push({title: 'ตอนที่ ' + num, file: file_name});
+	}
+	$scope.mp3_lists.push({title: 'กล่าวปิดท้าย ธรรมเตือนใจ', file:'00 กล่าวเกริ่นนำ ธ.ต.mp3'});
+  
+	function addZ(n) {
+		return (n<10? '00' : n<100? '0' : '') + n;
+	}
+});
