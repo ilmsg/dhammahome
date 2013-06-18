fe175b3800c6aec5215d7eac2ae723edeac8fb35
ilmsg <eak.netpanya@ilmsg.com>
2013-06-18 13:47:31 +0700

    edit path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('dhammahome', []);
 
 app.controller('MainCtrl', function($scope) {
-	$scope.mp3_folder = 'mp3/';
+	$scope.mp3_folder = './mp3/';
 	$scope.mp3_lists = [];
 	
 	$scope.mp3_lists.push({title: 'กล่าวเกริ่นนำ', file:'00 กล่าวเกริ่นนำ ธ.ต.mp3'});
